04c2465167bb4108776e674c817e2436a2e2c136
kumaraakarshan <aakarshan0005@gmail.com>
2023-09-19 21:47:54 +0530

    fetch done using async await

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React,{useState} from 'react';
-
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map(movieData => {
+
+  async function fetchMoviesHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films');
+      if (!response.ok) {
+        throw new Error('Failed to fetch movies.');
+      }
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.opening_crawl,
-          releaseData:movieData.release_date
-        }
+          releaseDate: movieData.release_date, // Corrected the variable name here
+        };
       });
+
       setMovies(transformedMovies);
-    })
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
   }
- 
 
   return (
     <React.Fragment>
